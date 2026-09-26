1968dc20ed969a922b435d6c6df6544b887831dd
Rıdvan <rdvn35050@gmail.com>
2026-09-26 14:23:54 +0300

    Migration: RecurringIntervalCount

---
+++ src/TaneHesap.Infrastructure/Migrations/20260926112354_RecurringIntervalCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaneHesap.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecurringIntervalCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IntervalCount",
+                table: "RecurringExpenses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IntervalCount",
+                table: "RecurringExpenses");
+        }
+    }
+}
